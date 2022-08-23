64e71d59ff4ffd67436e12a4f581a7f7f855f218
nandan vasista <nandan@Shreyass-MacBook-Pro.local>
2022-08-23 07:13:48 +0530

    added an error message

--- src/components/moreInfo/Code.jsx
+++ src/components/moreInfo/Code.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
-import { allCodes } from './AllCode';
-import './Code.css';
+import React, { useState } from "react";
+import { allCodes } from "./AllCode";
+import "./Code.css";
 const Code = ({ algoNumber }) => {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   const handleClick = (index) => {
     setCode(() => {
-      const text=allCodes[algoNumber][index].code
+      const text = allCodes[algoNumber][index].code;
       setCode(text);
     });
   };
-  const languages = ['Java', 'C++', 'C', 'JS', 'Python'];
+  const languages = ["Java", "C++", "C", "JS", "Python"];
   return (
     <span className="code">
       <span className="program">
         <span className="options">
           {algoNumber === 0 ? (
-            <h1>Hello Programmer</h1>
+            <>
+              <h1>Hello Programmer</h1>
+              <p style={{ color: "lightcoral" }}>
+                Click On Language Before Running the visualizer
+              </p>
+            </>
           ) : (
             languages.map((lang, index) => {
               return (
